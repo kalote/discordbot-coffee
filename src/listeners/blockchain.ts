b98a312cc7be4276aadbc22c5bc75ba58118f1bf
Johann BICH <johann@lukso.io>
2023-05-01 13:01:53 +0200

    fixing

--- src/listeners/blockchain.ts
+++ src/listeners/blockchain.ts
@@ -2,9 +2,11 @@ import { ethers } from "ethers";
 import { Channel, ChannelType, Client, EmbedBuilder } from "discord.js";
 import dotenv from "dotenv";
 import luksoABI from "../abi/LuksoGenesis.json";
+import USDTAbi from "../abi/USDTAbi.json";
 dotenv.config();
 
 const luksoAddress = "0x42000421dd80D1e90E56E87e6eE18D7770b9F8cC";
+const usdtAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 
 export default async (channel: Channel, client: Client): Promise<void> => {
   console.log("Listening on contract events ...");
@@ -12,55 +14,46 @@ export default async (channel: Channel, client: Client): Promise<void> => {
   const provider = new ethers.providers.WebSocketProvider(
     `wss://mainnet.infura.io/ws/v3/${process.env.INFURA_ID}`
   );
-  const contract = new ethers.Contract(luksoAddress, luksoABI, provider);
+  const contract = new ethers.Contract(usdtAddress, USDTAbi, provider);
 
-  contract.on(
-    "DepositEvent(bytes,bytes,uint256,bytes,uint256)",
-    (pubkey, withdrawal_credentials, amount, signature, index, event) => {
-      console.log("New Deposit! (signature)");
-      if (!client.user || !client.application) {
-        console.log("Error: Client.user or application is undefined");
-        return;
-      }
-      count += 1;
-      const lyxeStaked = count * 32;
-      const targetCompletion = ((count * 100) / 4096).toFixed(2);
-
-      const embed = new EmbedBuilder()
-        .setColor(0x41b983)
-        .setTitle("A new Genesis Validator joined 🔥")
-        .setAuthor({
-          name: client.user.username,
-          iconURL: client.user.displayAvatarURL(),
-        })
-        .setDescription(
-          `A new validator joined the LUKSO adventure at block \`${event.blockNumber}\` !`
-        )
-        .addFields(
-          { name: "Validator Count", value: `${count}`, inline: true },
-          { name: "LYXe Staked", value: `${lyxeStaked} LYXe`, inline: true },
-          {
-            name: "Target completion",
-            value: `${targetCompletion}%`,
-            inline: true,
-          }
-        );
-      // channel.send(`A new validator joined the LUKSO adventure at block \`${event.blockNumber}\` 🎉🎉🎉!\nWe now have ${count} validators (${count*32} LYXe) which represents ${(count*100/4096).toFixed(2)}% of our target!`);
-      if (channel.type == ChannelType.GuildText) {
-        channel.send({ embeds: [embed] });
-      }
-      console.log(JSON.stringify(event, null, 4));
+  contract.on("Transfer", (from, to, value, event) => {
+    // contract.on(
+    //   "DepositEvent(bytes,bytes,uint256,bytes,uint256)",
+    //   (pubkey, withdrawal_credentials, amount, signature, index, event) => {
+    console.log("New Transfer!");
+    if (!client.user || !client.application) {
+      console.log("Error: Client.user or application is undefined");
+      return;
     }
-  );
+    count += 1;
+    const lyxeStaked = count * 32;
+    const targetCompletion = ((count * 100) / 4096).toFixed(2);
 
-  const filter = {
-    address: luksoAddress,
-    topics: [
-      ethers.utils.id("DepositEvent(bytes,bytes,uint256,bytes,uint256)"),
-    ],
-  };
-
-  provider.on(filter, (log, event) => {
-    console.log("New Deposit! (provider)", event);
+    // const embed = new EmbedBuilder()
+    //   .setColor(0x41b983)
+    //   .setTitle("A new Genesis Validator joined 🔥")
+    //   .setAuthor({
+    //     name: client.user.username,
+    //     iconURL: client.user.displayAvatarURL(),
+    //   })
+    //   .setDescription(
+    //     `A new validator joined the LUKSO adventure at block \`${event.blockNumber}\` !`
+    //   )
+    //   .addFields(
+    //     { name: "Validator Count", value: `${count}`, inline: true },
+    //     { name: "LYXe Staked", value: `${lyxeStaked} LYXe`, inline: true },
+    //     {
+    //       name: "Target completion",
+    //       value: `${targetCompletion}%`,
+    //       inline: true,
+    //     }
+    //   );
+    if (channel.type == ChannelType.GuildText) {
+      channel.send(
+        `A new transfer happened on USDT contract \`${event.blockNumber}\` 🎉🎉🎉!`
+      );
+      // channel.send({ embeds: [embed] });
+    }
+    console.log(JSON.stringify(event, null, 4));
   });
 };
--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,4 +1,4 @@
-import { ChannelType, Client, EmbedBuilder } from "discord.js";
+import { ChannelType, Client, GuildMember } from "discord.js";
 import * as cron from "node-cron";
 import cronCoffee from "./cronCoffee";
 import blockchain from "./blockchain";
@@ -34,6 +34,44 @@ export default (client: Client): void => {
     // every Tuesday morning at 7am, a message will be posted in the
     // channel pairing members together
     // cron.schedule("*/5 * * * *", cronCoffee(channelCoffee));
-    // cron.schedule("0 7 * * TUE", cronCoffee(channelCoffee));
+    cron.schedule("*/2 * * * *", () => {
+      console.log("Cron executed!");
+
+      let memberArr: GuildMember[] = [];
+      if (channelCoffee.type === ChannelType.GuildText) {
+        channelCoffee.members
+          .filter((member) => !member.user.bot)
+          .each((member) => memberArr.push(member));
+
+        if (memberArr.length < 2) {
+          channelCoffee.send("There aren't enough members to pair up.");
+          return;
+        }
+
+        // Shuffle the members and pair them up
+        memberArr.sort((a, b) => Math.random() - 0.5);
+        const pairs: string[] = [];
+
+        memberArr.forEach((member, index) => {
+          if (index % 2 === 0) {
+            if (memberArr[index + 1] !== undefined) {
+              pairs.push(`${member.user} with ${memberArr[index + 1].user}`);
+            } else {
+              pairs.push(`${member.user} will have a coffee with me 🤖`);
+            }
+          }
+        });
+
+        const intro =
+          "Let's have random virtual coffee (or mate) to know each other better!\n\n☕️ Today's pairs are:\n\n";
+        const outro = "\n\nYour favorite bot 🤖";
+        const res = intro + pairs.join("\n") + outro;
+
+        channelCoffee.send(res);
+        return;
+      } else {
+        console.log("Error: Channel is not of type text");
+      }
+    });
   });
 };
